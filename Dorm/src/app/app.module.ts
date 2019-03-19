6648ef0cf306ce0096f916940d694e94a5e66411
DairovOlzhas <35361206+DairovOlzhas@users.noreply.github.com>
2019-03-20 00:57:55 +0600

    Add files via upload

--- Dorm/src/app/app.module.ts
+++ Dorm/src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { Routes, RouterModule} from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { PageComponent } from './page/page.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'cart', component: CartComponent},
+  {path: 'product', component: ProductComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'page', component: PageComponent},
+  {path: '**', component: NotFoundComponent},
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CartComponent,
+    ProductComponent,
+    ProductsComponent,
+    NotFoundComponent,
+    PageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
